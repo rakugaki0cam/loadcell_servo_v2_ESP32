cc619029061d5141f9de1da28735ed2e72b1c1d6
rakugaki0cam <isober21@gmail.com>
2023-03-29 21:01:37 +0900

    v2.32b

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-# 電気玉棒
-# 抜弾抵抗測定
+#
+# Electric Tama Bow
+#  Nukidan force measurement
 #
 # loadcell   HX711  500gf
 # servo motor SG92R
@@ -7,11 +8,12 @@
 # ESP32
 #
 # 2022.11.27
-# 2023.01.05          SDcard 追加
-# 2023.03.25          SDcardなし　基板制作（一部ピン変更）
-# 2023.03.26 ver.2.30 やっぱりSDcard あり
-# 2023.03.27 ver.2.31 WiFiより時刻を取得->SDcardへの保存ファイルのタイムスタンプが入る
-# 2023.03.28 ver.2.32 ファイル名に日時を入れてユニークに
+# 2023.01.05            SDcard add
+# 2023.03.25            No SDcard  Build board（some GPIO pin position　change）
+# 2023.03.26 ver.2.30   re-add SDcard
+# 2023.03.27 ver.2.31   Get Time from WiFi -> SDcard time stamp
+# 2023.03.28 ver.2.32   filename <- date&time add
+# 2023.03.29 ver.2.32b  delete Japanese Font ->  can not use in pymakr
 #
 #
 
@@ -21,7 +23,7 @@ import uos
 import ntptime
 import utime
 from machine import Pin, PWM, SDCard, RTC
-import wifiid
+import wifiid       # wifiid.py - WIFI_SS_ID, WIFI_PASSWORD
 
 print()
 print("***** Denki Tamabo *************************")
@@ -67,9 +69,8 @@ blueLed.off()
 
 
 # *** SD card *************************************************
-cd = Pin(22, mode = Pin.IN, pull = Pin.PULL_UP)    #SDカードが入っていないとエラーで止まるのでcdピン追加
+cd = Pin(22, mode = Pin.IN, pull = Pin.PULL_UP)    #Card Detect pin add
 sd = SDCard(slot=2, width=1, sck=Pin(18), mosi=Pin(23), miso=Pin(19), cs=Pin(5))
-# Hard ResetしてESP32内のプログラムを実行ではOKだけど、VScodeからのrunだとエラーになって止まる???　OSError: (-259, 'ESP_ERR_INVALID_STATE')
 if (cd.value() == 0):
     detectSd = 1
     uos.mount(sd, '/sd')
@@ -99,19 +100,19 @@ utime.sleep_ms(1)
 hx711Clock.off()
 utime.sleep_ms(1)
 
-V_out = 0.000728            # センサ出力電圧@maxLoad [V/V]  spec:0.65~0.95mV 校正対象
-maxLoad = 500.0             # センサ定格 [g]
+V_out = 0.000728            # sensor Voltage@maxLoad [V/V]  spec:0.65~0.95mV
+maxLoad = 500.0             # sensor rating [g]
 #ADConverter config
-ADCGain = 128               # A/Dゲイン
-R1 = 12000                 # R1抵抗値 [Ω] 3.3V版に改造
-R2 = 8200                  # R2抵抗値 [Ω]
-Vbg = 1.25                 # アナログリファレンス電圧 [V]
-aVdd = Vbg * (R1+R2)/R2    # アナログ電圧フルスケールAVDD [V]
-ADC1bit = aVdd / 2**24     # フルスケールを24bitで割る [V]
-Scale = V_out / maxLoad * aVdd  # ロードセル特性　[V/gf]
-k1 = ADC1bit / (Scale * ADCGain) #計算の係数
+ADCGain = 128               # A/D　gain
+R1 = 12000                  # Register1 [Ω] for 3.3V
+R2 = 8200                   # Register2 [Ω]
+Vbg = 1.25                  # analog reference [V]
+aVdd = Vbg * (R1+R2)/R2     # analog max voltage AVDD [V]
+ADC1bit = aVdd / 2**24      # max voltage / 24bit [V/bit]
+Scale = V_out / maxLoad * aVdd  # loadcell scale [V/gf]
+k1 = ADC1bit / (Scale * ADCGain)
 #
-k = maxLoad / (V_out * 2**24 * ADCGain) #式を整理 gf/data1bit
+k = maxLoad / (V_out * 2**24 * ADCGain) # gf/data1bit
 
 print('# loadcell')
 print(f'loadcell:         {maxLoad:4.0f} gf (MAX)')
@@ -141,13 +142,13 @@ def readAdc():
     while True:
         # ADc wait
         if hx711Data.value() == 0:
-            utime.sleep_us(100)      #78usほどの変なパルスがあるらしいので除去
+            utime.sleep_us(100)      # remove 78us pulse noise
             if hx711Data.value() == 0:
                 break
 
     for _ in range(24):
         hx711Clock.on()
-        #utime.sleep_us(1) #遅延いれなくても10usほど
+        #utime.sleep_us(1)  # no delay -> over 10us 
         hx711Clock.off()
         adValue = adValue << 1
         adValue += hx711Data.value()
@@ -155,7 +156,7 @@ def readAdc():
     hx711Clock.on()
     hx711Clock.off()
     if (adValue & 0x00800000) != 0:
-        # マイナスの時の処理(24ビット数)
+        # minus value (24bit)
         adValue = (adValue ^ 0xfffffe) * -1
     # print(f"{adValue:06x}", end = '  ')  # debug
     return adValue
@@ -180,7 +181,7 @@ def tareZero():
 
 #test
 def testLoadcell():
-    # ロードセルの連続表示　テスト用
+    # loadcell continuous test
     zeroV = tareZero()
     while True:
         wgf = digiVtoWeight(readAdc(), zeroV)
@@ -191,11 +192,11 @@ def testLoadcell():
 servo1 = PWM(Pin(4), freq=50)  # PWM freq 1~1000Hz
 # servo 50Hz = 20msec
 # global
-armR = 8.5      # サーボホーンの腕の長さ[mm]
-startDeg = -24	# 初期角度[°]
-endDeg = 26	    # 終角度[°]
-incDeg = 1.0    # 角度増分[°]
-degOffset = 6   # サーボ中立電圧位置でのズレ[°]
+armR = 8.5      # servo horn length[mm]
+startDeg = -24	# start angle[°]
+endDeg = 26	    # end angle[°]
+incDeg = 1.0    # increment angle[°]
+degOffset = 6   # offset at neutral position[°]
 stepbyDeg = armR * math.sin(math.radians(1))    # mm/deg
 startPos = armR * math.sin(math.radians(startDeg))  #mm
 print('# servo')
@@ -214,14 +215,14 @@ def servoMoveDeg(deg1):
 
 def servoDegtoHex(setDeg):
     if (setDeg < (startDeg-1)) or (setDeg > (endDeg+1)):
-        # 可動範囲外の時リターン
+        # out of range
         print("errorroror")
         return -9.9
     v0_u16 = 74*64        # 1.45msec = 7.25%
     vn90_u16 = 120*64      # 2.4msec = 12.0%
     vp90_u16 = 30*64       # 0.5msec = 2.5%
 
-    degP = setDeg + degOffset                   # サーボへの指令角度
+    degP = setDeg + degOffset                   # servo angle
     value_u16 = -degP * (vn90_u16 - vp90_u16) / 180 + v0_u16
     #print(value_u16, end=' ')  # debug
     return int(value_u16)
@@ -241,34 +242,34 @@ def servoPos():
 
     while True:
         print('zero position')
-        servo1.duty_u16(servoDegtoHex(0))           # サーボ　ゼロ位置
+        servo1.duty_u16(servoDegtoHex(0))           # servo neutral position
         utime.sleep(1)
         while True:
             if tSecWait(1) == 1:
                 break
 
         print('start position')
-        servo1.duty_u16(servoDegtoHex(startDeg))    # サーボ　スタート位置
+        servo1.duty_u16(servoDegtoHex(startDeg))    # servo start position
         utime.sleep(1)
         while True:
             if tSecWait(1) == 1:
                 break
 
         print('end position')
-        servo1.duty_u16(servoDegtoHex(endDeg))      # サーボ　エンド位置
+        servo1.duty_u16(servoDegtoHex(endDeg))      # servo end position
         utime.sleep(1)
         while True:
             if tSecWait(1) == 1:
                 break
 
 def servoMove():
-    # サーボを動かして見てみる
+    # servo move test
     print('SERVO TEST   push button to 1step rotate')
-    posDeg = 0.0  # スタート角度
-    degInc = 0.5  # 変化分
+    posDeg = 0.0  # start angle
+    degInc = 0.5  # increment step
     d = 0
     while True:
-        d = servoDegtoHex(posDeg)     #エラーの時-9.9で帰るのでtypeErrorで止まる
+        d = servoDegtoHex(posDeg)     # return val = -9.9 in err, stop program by "typeError"
         servo1.duty_u16(d)
         print(f"{posDeg:5.1f}deg   duty={(d/655.36):5.2f}%  ({d:4d}/65536)")
         utime.sleep_ms(150)
@@ -281,7 +282,7 @@ def servoMove():
             degInc = -degInc
 
 
-# *** 入力 セーブ等 サブ **************************************************
+# *** Input , Data Save subroutine **************************************************
 def tSecWait(t):
     #t秒のボタン入力待ち
     for _ in range(t * 10):
@@ -292,13 +293,12 @@ def tSecWait(t):
 
 
 def dataSave(da):
-    # データを保存
+    # data save -> SD card
     if(detectSd == 0):
         print('!!! No SD card !!! can not data save.')
         return
     
     f = open(fileName, 'a')
-    #f = open('denkitamabou.txt', 'a') #ESPフラッシュへ書込
     f.write(f"No., time[sec], arm[deg], x_pos[mm], force[gf]\n")
     for d in da:
         f.write(f"{d[0]:3d},   {d[1]:7.3f},   {d[2]:6.2f},   {d[3]:7.3f},  {d[4]:8.2f}\n")
@@ -307,12 +307,13 @@ def dataSave(da):
 
 
 # *** test test test ******************************************
-#ZeroOffset = tare()    # 秤のゼロ合わせ
-#testLoadcell()         # loadcell 連続表示テスト
-#servoPos()             # サーボの位置
-#servoMove()            # サーボを動かしてみる
+#ZeroOffset = tare()    # Loadcell Zero set
+#testLoadcell()         # loadcell Continuous test
+#servoPos()             # servo position
+#servoMove()            # servo move test
+#
 #data = [[1, 1.0, 1.0, 1.0],[2, 1.2, 2.0, 2.5]]
-#ataSave(data)              #データ保存
+#dataSave(data)         #data save test
 
 
 # ***  MAIN ***************************************************
@@ -348,20 +349,20 @@ while True:
     print()
     # ready LED on
     blueLed.on()
-    #サーボ初期位置
+    # servo start position
     deg = servoDegtoHex(startDeg)
     servo1.duty_u16(deg)
     utime.sleep_ms(1000)
-    # ここで重量表示できると良いかも
+    # ----> weight display , if possible 
 
     print('push button to start measurement')
     while True:
         if sw1.value() == 0:
             break
 
-    # 測定開始
+    # start measurement
     blueLed.off()
-    zeroOffset = tareZero()  # 毎回ゼロ合わせ
+    zeroOffset = tareZero()  # set zero every time
     utime.sleep_ms(2)
 
     deg = startDeg
@@ -380,10 +381,10 @@ while True:
         #print(f"{deg:7.2f} deg   ", end = "")  # degree
         print(f"{posMm:6.2f}    ", end = "")    # position x
 
-        utime.sleep_ms(20)        # サーボのタイムラグ PWM信号を受けるまでの時間と回す時間
+        utime.sleep_ms(20)        # servo time lag (receive PWM signal & moving)
         utime.sleep_ms(waitFor)
 
-        adV = averageData(numAve, 1)            # 測定周期 80Hz(実測10.6msecくらい)
+        adV = averageData(numAve, 1)            # measurement cycle 80Hz　(Actual measurement　10.6msec)
         weight = digiVtoWeight(adV, zeroOffset)
 
         print(f"   {weight:6.1f}")      # weight
